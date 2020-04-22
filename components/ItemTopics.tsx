c32e912e3c5bedb29e5c8eb19bb1052ba09d5e2e
Oleksii Kovalchuk <oleksiij.ko@gmail.com>
2020-04-22 21:21:38 +0300

    Fix

--- components/ItemTopics.tsx
+++ components/ItemTopics.tsx
@@ -7,13 +7,14 @@ interface IProps {
 const ItemTopics: React.FC<IProps> = ({ topics }) => {
   return (
     <div className="topics">
-      {topics.map((topic, index) => {
-        return (
-          <a key={index} href={`https://github.com/topics/${topic}`}>
-            <div className="topics__topic">{topic}</div>
-          </a>
-        );
-      })}
+      {topics &&
+        topics.map((topic, index) => {
+          return (
+            <a key={index} href={`https://github.com/topics/${topic}`}>
+              <div className="topics__topic">{topic}</div>
+            </a>
+          );
+        })}
     </div>
   );
 };
