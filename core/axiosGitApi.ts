7bae56d039891dfe35c36611862329a9d5b9e4b6
Oleksii Kovalchuk <oleksiij.ko@gmail.com>
2020-04-22 21:11:14 +0300

    make it better

--- core/axiosGitApi.ts
+++ core/axiosGitApi.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 
 axios.defaults.headers.common["Accept"] =
   "application/vnd.github.mercy-preview+json";
-axios.defaults.headers.common[
-  "Authorization"
-] = `Bearer 22f0c67ba87ca65248b5f1cf5cbe723d98720100`;
+// axios.defaults.headers.common[
+//   "Authorization"
+// ] = `Bearer 22f0c67ba87ca65248b5f1cf5cbe723d98720100`;
 
 axios.defaults.baseURL = "https://api.github.com";
 
